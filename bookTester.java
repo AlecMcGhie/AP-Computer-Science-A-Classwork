a01dd83db79ab6adea3e035d2b32e42729186f17
AlecMcGhie <40875085+AlecMcGhie@users.noreply.github.com>
2019-11-13 09:32:06 -0800

    Add files via upload

---
+++ bookTester.java
@@ -0,0 +1,10 @@
+
+public class bookTester
+{
+    Book B1 = new Book("My Cool Book","Alec McGhie",345);
+    Book B2 = new Book("My Bad Book","Henry", 342);
+    System.out.println(B1.toString());
+    
+
+    
+}
